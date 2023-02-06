0b994abb2f3ee6e27b5a8cb3434e2a2f86b107ac
NataliaPopova127 <pnatala127@mail.ru>
2023-02-07 00:31:22 +0300

    add icon

--- app/src/main/java/com/example/mycollegemobileapp/AuthorizationActivity.kt
+++ app/src/main/java/com/example/mycollegemobileapp/AuthorizationActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mycollegemobileapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AuthorizationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_MyCollegeMobileApp)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authorization)
+        title = "Авторизация"
     }
+
+    fun btnApplicantInfClick(view: View) {
+        var intent : Intent = Intent(this@AuthorizationActivity, ApplicantInfActivity::class.java)
+        startActivity(intent)
+    }
+    fun btnSignInClick(view: View) {}
 }
